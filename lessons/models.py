87c7fddee0ca51fbb460fcc5730b201d3549b394
RonilRufo <ronil.rufo@gmail.com>
2014-09-10 17:29:53 +0800

    Modified fetching of Youtube and Vimeo details

--- lessons/models.py
+++ lessons/models.py
@@ -1,5 +1,8 @@
 import requests
+import gdata.youtube
+import gdata.youtube.service
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
@@ -47,13 +50,36 @@ class VideoLesson(OrderedModel):
         verbose_name = "Video Lesson"
         verbose_name_plural = "Video Lessons"
 
+    # def _get_youtube_info(self, backend):
+    #     info = ""
+    #     try:
+    #         req = requests.get("http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%s&format=json" % (backend.code))
+    #         info = req.text
+    #     except:
+    #         pass
+    #     return info
+
+    def _generate_youtube_info(self, entry):
+        author = entry.author[0].name.text if entry.author and entry.author[0] else ""
+        info = {
+            "title": entry.media.title.text,
+            "published_date": entry.published.text,
+            "description": entry.media.description.text,
+            "view_count": entry.statistics.view_count,
+            "duration": entry.media.duration.seconds,
+            "author": author
+        }
+        return info
+
     def _get_youtube_info(self, backend):
-        info = ""
-        try:
-            req = requests.get("http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%s&format=json" % (backend.code))
-            info = req.text
-        except:
-            pass
+        yt_service = gdata.youtube.service.YouTubeService()
+        yt_service.ssl = True
+        yt_service.developer_key = settings.GOOGLE_DEVELOPER_KEY
+        yt_service.client_id = settings.GOOGLE_CLIENT_ID
+
+        entry = yt_service.GetYouTubeVideoEntry(video_id=backend.code)
+        info = self._generate_youtube_info(entry)
+
         return info
 
     def save(self, *args, **kwargs):
@@ -61,8 +87,14 @@ class VideoLesson(OrderedModel):
 
         if not self.info:
             backend = detect_backend(self.video)
-            try:
-                self.info = backend.info
-            except:
-                self.info = self._get_youtube_info(backend)
+
+            if backend.backend == "VimeoBackend":
+                info = backend.info
+            elif backend.backend == "YoutubeBackend":
+               info = self._get_youtube_info(backend)
+               info.update({"id": backend.code})
+
+            info.update({"backend": backend.backend})
+            info.update({"thumbnail": backend.thumbnail})
+            self.info = info
             self.save()
