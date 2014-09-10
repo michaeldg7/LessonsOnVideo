f7a2c6041291c90961445f60e9b6e17966ea47b2
RonilRufo <ronil.rufo@gmail.com>
2014-09-10 18:51:13 +0800

    Removed unnecessary fields in admin and fixed title display on index page

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,3 @@
-import ast
-
 from django.contrib import admin
 
 from embed_video.admin import AdminVideoMixin
@@ -24,35 +22,24 @@ class VideoLessonAdmin(AdminVideoMixin, OrderedModelAdmin):
     """
     model = VideoLesson
 
-    list_display = ('get_title', 'order', 'move_up_down_links')
+    list_display = ('title', 'order', 'move_up_down_links')
     ordering = ('order', )
 
-    readonly_fields = ['get_title', ]
     fieldsets = (
         ('Owner', {
             'fields': ('user', )
         }),
         ('Video Info', {
-            'fields': ('get_title', 'video', 'category', 'info')
+            'fields': ('video', 'category')
         }),
         )
 
-    def get_title(self, obj):
-        title = "[No Title]"
-        if obj and obj.info:
-            try:
-                info = ast.literal_eval(obj.info)
-                title = info['title']
-            except:
-                pass
-        return title
-    get_title.short_description = "Title"
 
-    def get_readonly_fields(self, request, obj=None):
-        readonly_fields = self.readonly_fields
-        if obj:
-            readonly_fields += ['get_title']
-        return readonly_fields
+    # def get_readonly_fields(self, request, obj=None):
+    #     readonly_fields = list(self.readonly_fields)
+    #     if obj:
+    #         readonly_fields += ['title', 'thumbnail']
+    #     return readonly_fields
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(VideoLesson, VideoLessonAdmin)
