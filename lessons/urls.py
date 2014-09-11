6319d2318d5a02f18d86bfa7228f98b5ccf33811
RonilRufo <ronil.rufo@gmail.com>
2014-09-11 16:49:33 +0800

    Fixed breadcrumb and added category videos page

--- lessons/urls.py
+++ lessons/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lessons.views',
         url(r'^video/(?P<video_slug>.*\w+)/$', 'video_detail', name="video_detail"),
+        url(r'^category/(?P<category_slug>.*\w+)/$', 'category_videos', name="category_videos"),
     )
--- lessons/views.py
+++ lessons/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
-from lessons.models import VideoLesson
+from lessons.models import VideoLesson, Category
 
 
 def homepage(request, template_name="index.html"):
     """
     Displays the home page
+
+    **Context**
+
+        ''lessons''
+            A queryset of all 'lessons.VideoLesson' objects.
+
+        ''RequestContext
+
+    **Template:""
+
+        :template:'index.html'
+
     """
     lessons = VideoLesson.objects.all()
     context = {
@@ -30,9 +42,39 @@ def video_detail(request, video_slug, template_name="lessons/video.html"):
     **Template:""
 
         :template:'lessons/video.html'
+
     """
     lesson = get_object_or_404(VideoLesson, slug=video_slug)
     context = {
         "lesson": lesson
+    }
+    return render_to_response(template_name, context, RequestContext(request))
+
+
+def category_videos(request, category_slug, template_name="lessons/category_videos.html"):
+    """
+    Displays 'lessons.VideoLesson' videos based on the given 'category_slug' parameter.
+    This page will also display products based on the given category.
+
+    **Context**
+
+        ''category''
+            An instance of 'lessons.Category'
+
+        ''lessons''
+            A queryset of 'lessons.VideoLesson' objects.
+
+        ''RequestContext
+
+    **Template:""
+
+        :template:'lessons/category_videos.html'
+
+    """
+    category = get_object_or_404(Category, slug=category_slug)
+    lessons = VideoLesson.objects.filter(category=category)
+    context = {
+        "category": category,
+        "lessons": lessons
     }
     return render_to_response(template_name, context, RequestContext(request))
