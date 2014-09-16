79140a287533c966b8f2e90322d0dda21263b402
RonilRufo <ronil.rufo@gmail.com>
2014-09-16 10:38:02 +0800

    Implemented aJax pagination on category page

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lessons.views',
+        # aJax Calls
+        url(r'^category/ajax/(?P<category_slug>.*\w+)/$', 'category_videos_ajax',
+            name="category_videos_ajax"),
+
         url(r'^video/(?P<video_slug>.*\w+)/$', 'video_detail', name="video_detail"),
         url(r'^category/(?P<category_slug>.*\w+)/$', 'category_videos', name="category_videos"),
     )
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
@@ -72,9 +73,41 @@ def category_videos(request, category_slug, template_name="lessons/category_vide
 
     """
     category = get_object_or_404(Category, slug=category_slug)
-    lessons = VideoLesson.objects.filter(category=category)
     context = {
-        "category": category,
+        "category": category
+    }
+    return render_to_response(template_name, context, RequestContext(request))
+
+
+def category_videos_ajax(request, category_slug, template_name="lessons/ajax/category_videos_ajax.html"):
+    """
+    Displays 'lessons.VideoLesson' videos based on the given 'category_slug' parameter through aJax.
+
+    **Context**
+
+        ''lessons''
+            A queryset of 'lessons.VideoLesson' objects.
+
+        ''RequestContext
+
+    **Template:""
+
+        :template:'lessons/ajax/category_videos_ajax.html'
+
+    """
+    category = get_object_or_404(Category, slug=category_slug)
+    lessons_list = VideoLesson.objects.filter(category=category)
+
+    page = request.GET.get('page')
+    try:
+        paginator = Paginator(lessons_list, 6)
+        lessons = paginator.page(page)
+    except PageNotAnInteger:
+        lessons = paginator.page(1)
+    except EmptyPage:
+        lessons = paginator.page(paginator.num_pages)
+
+    context = {
         "lessons": lessons
     }
     return render_to_response(template_name, context, RequestContext(request))
