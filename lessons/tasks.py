a0d0e49679bd624250b36297838a5c0785756903
RonilRufo <ronil.rufo@gmail.com>
2014-10-07 22:08:47 +0800

    Fixed bug on URL uploads

--- lessons/tasks.py
+++ lessons/tasks.py
@@ -2,6 +2,7 @@ import requests
 import urllib
 
 from django.conf import settings
+from django.db import transaction
 
 from lessons.models import VideoLesson
 
@@ -26,16 +27,19 @@ def create_videos_via_playlist(creator, playlist_url, category):
 
     counter = 0
     logger.info(">>>>> Start creating videos")
+    remove_list = []
     for url in req.iter_lines():
         try:
-            VideoLesson.objects.create(
-                user=creator,
-                video=url,
-                category=category
-            )
+            lesson = VideoLesson()
+            lesson.user = creator
+            lesson.video = url
+            lesson.category = category
+            lesson.save()
             counter += 1
         except:
             error_msg = ">>>>> Could not create video in URL: %s" % (url, )
+            remove_list.append(url)
             logger.info(error_msg)
 
+    VideoLesson.objects.filter(video__in=remove_list).delete()  # Hack to remove URLs that throws exception
     logger.info(">>>>> Successfully Created %s video/s." % (counter, ))
