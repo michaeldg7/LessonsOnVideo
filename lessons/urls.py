f8eda586ae4ce832b350b228787ede56cad19f55
Mateusz Szefer <szefer.mateusz@gmail.com>
2014-11-28 10:49:07 +0100

    Fixes slug error - Long time process on the 4th page in Russian lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lessons.views',
         # aJax Calls
-        url(r'^category/ajax/(?P<category_slug>.*\w+)/$', 'category_videos_ajax',
+        url(r'^category/ajax/(?P<category_slug>[\w\-]+)/$', 'category_videos_ajax',
             name="category_videos_ajax"),
 
-        url(r'^video/(?P<video_slug>.*\w+)/$', 'video_detail', name="video_detail"),
-        url(r'^category/(?P<category_slug>.*\w+)/$', 'category_videos', name="category_videos"),
+        url(r'^video/(?P<video_slug>[\w\-]+)/$', 'video_detail', name="video_detail"),
+        url(r'^category/(?P<category_slug>[\w\-]+)/$', 'category_videos', name="category_videos"),
         url(r'^videos/create/$', 'create_playlist_videos', name="create_playlist_videos"),
     )
